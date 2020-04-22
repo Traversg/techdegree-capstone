30ae93084fc57b9a7cb224dd4265b4792d5b1cab
Traversg <57022409+Traversg@users.noreply.github.com>
2020-04-22 17:59:00 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -6,10 +6,12 @@ const githubIcon = document.querySelector('.github');
 emailIcon.addEventListener('click', e => {
     const element = e.target;
     element.style.fillOpacity = ".4"
+    element.style.transform = "scale(1.3)"
 });
 
 githubIcon.addEventListener('click', e => {
     const element = e.target;
     element.style.fillOpacity= ".4"
+    element.style.transform = "scale(1.3)"
 });
 
